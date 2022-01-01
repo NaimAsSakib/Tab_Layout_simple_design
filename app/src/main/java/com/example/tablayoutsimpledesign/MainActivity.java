9c025bb63e4686f26fb8b2f1d8f6d4a3c41e98bf
NaimAsSakib <rifat.sakib90@gmail.com>
2022-01-01 11:09:59 +0600

    Initial commit

---
+++ app/src/main/java/com/example/tablayoutsimpledesign/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.tablayoutsimpledesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+    TabLayout tabLayout;
+    TabItem tabItem1, tabItem2, tabItem3;
+    ViewPager viewPager;
+    PageAdapter pageAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout=findViewById(R.id.tab_Layout1);
+        tabItem1=findViewById(R.id.tabItem1);
+        tabItem2=findViewById(R.id.tabItem2);
+        tabItem3=findViewById(R.id.tabItem3);
+        viewPager=findViewById(R.id.vPager);
+
+        pageAdapter=new PageAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+        viewPager.setAdapter(pageAdapter);
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+
+                
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        //for scroll or page change in viewpager
+
+    }
+}
---
+++ app/src/main/java/com/example/tablayoutsimpledesign/PageAdapter.java
@@ -0,0 +1,31 @@
+package com.example.tablayoutsimpledesign;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class PageAdapter extends FragmentPagerAdapter {
+    int tabCount;
+    public PageAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+        tabCount=behavior;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0: return new ftab1();
+            case 1: return new ftab2();
+            case 2: return new ftab3();
+            default: return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return tabCount;
+    }
+}
+
