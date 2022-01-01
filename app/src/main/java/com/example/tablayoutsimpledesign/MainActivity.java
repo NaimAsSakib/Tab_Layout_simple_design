fcc22ddc6f7fa8a5b6e6ca99f7405c853585fb86
NaimAsSakib <rifat.sakib90@gmail.com>
2022-01-01 11:10:39 +0600

    Added if

--- app/src/main/java/com/example/tablayoutsimpledesign/MainActivity.java
+++ app/src/main/java/com/example/tablayoutsimpledesign/MainActivity.java
@@ -33,8 +33,8 @@ public class MainActivity extends AppCompatActivity {
             public void onTabSelected(TabLayout.Tab tab) {
                 viewPager.setCurrentItem(tab.getPosition());
 
-                
-
+                if ( tab.getPosition()==0 ||tab.getPosition()==1 || tab.getPosition()==2 )
+                    pageAdapter.notifyDataSetChanged();
             }
 
             @Override
